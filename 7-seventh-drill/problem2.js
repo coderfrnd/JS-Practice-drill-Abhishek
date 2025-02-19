555a932ff1cc35cb2244a08b3bc25d443696d459
Abhishek <warriorsshadow39@gmail.com>
2025-02-19 12:17:55 +0530

    problem 2 done

--- 7-seventh-drill/problem2.js
+++ 7-seventh-drill/problem2.js
@@ -1,97 +1,71 @@
 import fs from "fs";
 import path from "path";
 
-/*
-    Problem 2:
-    
-    Using callbacks and the fs module's asynchronous functions, do the following:
-        1. Read the given file lipsum.txt
-        2. Convert the content to uppercase & write to a new file. Store the name of the new file in filenames.txt
-        3. Read the new file and convert it to lower case. Then split the contents into sentences. Then write it to a new file. Store the name of the new file in filenames.txt
-        4. Read the new files, sort the content, write it out to a new file. Store the name of the new file in filenames.txt
-        5. Read the contents of filenames.txt and delete all the new files that are mentioned in that list simultaneously.
-*/
-
-function readTheFile(folderPath) {
-  fs.readFile(folderPath, "utf-8", (err, data) => {
-    if (err) {
-      console.log("Something wrong in reading file of lipsum.txt", err);
-    } else {
-      upperCase(data, (err, upperCaseFileName) => {
-        if (err) {
-          console.log("Error in Upper Case File name", err);
-        } else {
-          console.log("File Creation done", upperCaseFileName);
-          appendAnyfile(upperCaseFileName, (err) => {
-            if (err) {
-              console.log("Error in Upper Case File name", err);
-            } else {
-                readAnyFile(upperCaseFileName,(err,data)=>{
-                   if(err){
-                    console.log("error",err);
-                   }
-                   else{
-                   lowerCase(data,(err,lowerCaseFileName)=>{
-                    if(err){
-                        console.log("Error",err);
-                    }
-                    else{
-                        console.log("Suceessful",lowerCaseFileName);  
-                        appendAnyfile(lowerCaseFileName,(err)=>{
-                            if(err){
-                                console.log("Error in append file name",err);
-                            }
-                            else{
-                                console.log("Done append lower Case");                              
-                            }
-                        })              
-                    }
-                   })
-                    
-                   }
-                })
-
-            }
-          });
-        }
-      });
-    }
-  });
-}
-readTheFile("lipsum.txt");
-
-function readAnyFile(filePath, cb) {
+export function readAnyFile(filePath, cb) {
   fs.readFile(filePath, "utf-8", cb);
 }
 
-function upperCase(data, cb) {
+ export function upperCase(data,upperCaseFileName ,cb) {
   let upperCaseContent = data.toUpperCase();
-  let upperCaseFileName = "upperCase.txt";
   writeAnyFile(upperCaseFileName, upperCaseContent, (err) => {
     if (err) {
-        cb(err);
-    }
-    else {
-        cb(null, upperCaseFileName);
+      cb(err);
+    } else {
+      cb(null, upperCaseFileName);
     }
   });
 }
-function lowerCase(data,cb) {
+export function lowerCase(data, lowerCaseFileName,cb) {
   let lowerCaseContent = data.toLowerCase();
   lowerCaseContent = lowerCaseContent.split(".").join("\n");
-  let lowerCaseFileName = "lower.txt";
+  // let lowerCaseFileName = "lower.txt";
   writeAnyFile(lowerCaseFileName, lowerCaseContent, (err) => {
-    if (err){
-         cb(err);
-        }
-    else {
-        cb(null, lowerCaseFileName);
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, lowerCaseFileName);
     }
   });
 }
-function writeAnyFile(filePath, data, cb) {
+export function writeAnyFile(filePath, data, cb) {
   fs.writeFile(filePath, data, cb);
 }
-function appendAnyfile(data, cb) {
-  return fs.appendFile("filename.txt", data, cb);
+export function appendAnyfile(data,fileName ,cb) {
+  return fs.appendFile(fileName, data + "\n", cb);
+}
+
+  export function sortFile(data,sortedFileName,cb){
+  let sortedData = data
+  .split(" ")
+  .sort()
+  .join("\n");
+  writeAnyFile(sortedFileName,sortedData,(err)=>{
+    if(err)cb(err)
+    else{
+    cb(null,sortedFileName)
+  }  
+  })
+}
+
+export function deleteAllFile(fileName,cb){
+   readAnyFile(fileName,(err,data)=>{
+    if(err){
+      cb(err)
+    }
+    else{
+  let txtFileName =  data.split("\n")
+        txtFileName = txtFileName.filter(ele => ele.trim()!== "") 
+        txtFileName.forEach((fileNameIndex)=>{
+          fs.rm(fileNameIndex,(err,msg)=>{
+            if(err){
+              cb(err)
+            }
+            else{
+              cb(null,"done")
+            }
+          })
+   })
+    }
+   })
+  
 }

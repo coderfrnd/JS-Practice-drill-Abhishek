3e3aa44edb59c0daf8d918a92d16075521dfb8c0
Abhishek <warriorsshadow39@gmail.com>
2025-02-19 12:49:08 +0530

    second problem append function inside lower and upper and sorted done

--- 7-seventh-drill/problem2.js
+++ 7-seventh-drill/problem2.js
@@ -1,21 +1,28 @@
 import fs from "fs";
 import path from "path";
 
+let fileName = "filename.txt"
+
 export function readAnyFile(filePath, cb) {
   fs.readFile(filePath, "utf-8", cb);
 }
 
- export function upperCase(data,upperCaseFileName ,cb) {
+export function upperCase(data, upperCaseFileName, cb) {
   let upperCaseContent = data.toUpperCase();
   writeAnyFile(upperCaseFileName, upperCaseContent, (err) => {
     if (err) {
       cb(err);
     } else {
-      cb(null, upperCaseFileName);
+      appendAnyfile(upperCaseFileName, fileName, (err) => {
+        if (err) return cb(err);
+        else {
+          return cb(null, "done it");
+        }
+      });
     }
   });
 }
-export function lowerCase(data, lowerCaseFileName,cb) {
+export function lowerCase(data, lowerCaseFileName, cb) {
   let lowerCaseContent = data.toLowerCase();
   lowerCaseContent = lowerCaseContent.split(".").join("\n");
   // let lowerCaseFileName = "lower.txt";
@@ -23,49 +30,54 @@ export function lowerCase(data, lowerCaseFileName,cb) {
     if (err) {
       cb(err);
     } else {
-      cb(null, lowerCaseFileName);
+      appendAnyfile(lowerCaseFileName,fileName, (err) => {
+        if (err) return cb(err);
+        else {
+          return cb(null, "done it");
+        }
+      });
     }
   });
 }
 export function writeAnyFile(filePath, data, cb) {
   fs.writeFile(filePath, data, cb);
 }
-export function appendAnyfile(data,fileName ,cb) {
+export function appendAnyfile(data, fileName, cb) {
   return fs.appendFile(fileName, data + "\n", cb);
 }
 
-  export function sortFile(data,sortedFileName,cb){
-  let sortedData = data
-  .split(" ")
-  .sort()
-  .join("\n");
-  writeAnyFile(sortedFileName,sortedData,(err)=>{
-    if(err)cb(err)
-    else{
-    cb(null,sortedFileName)
-  }  
-  })
+export function sortFile(data, sortedFileName, cb) {
+  let sortedData = data.split(" ").sort().join("\n");
+  writeAnyFile(sortedFileName, sortedData, (err) => {
+    if (err) cb(err);
+    else {
+      // cb(null,sortedFileName)
+      appendAnyfile(sortedFileName, fileName, (err, msg) => {
+        if (err) return cb(err);
+        else {
+          return cb(null, "done it ");
+        }
+      });
+    }
+  });
 }
 
-export function deleteAllFile(fileName,cb){
-   readAnyFile(fileName,(err,data)=>{
-    if(err){
-      cb(err)
+export function deleteAllFile(fileName, cb) {
+  readAnyFile(fileName, (err, data) => {
+    if (err) {
+      cb(err);
+    } else {
+      let txtFileName = data.split("\n");
+      txtFileName = txtFileName.filter((ele) => ele.trim() !== "");
+      txtFileName.forEach((fileNameIndex) => {
+        fs.rm(fileNameIndex, (err, msg) => {
+          if (err) {
+            cb(err);
+          } else {
+            cb(null, "done");
+          }
+        });
+      });
     }
-    else{
-  let txtFileName =  data.split("\n")
-        txtFileName = txtFileName.filter(ele => ele.trim()!== "") 
-        txtFileName.forEach((fileNameIndex)=>{
-          fs.rm(fileNameIndex,(err,msg)=>{
-            if(err){
-              cb(err)
-            }
-            else{
-              cb(null,"done")
-            }
-          })
-   })
-    }
-   })
-  
+  });
 }
--- 7-seventh-drill/test/testProblem2.js
+++ 7-seventh-drill/test/testProblem2.js
@@ -1,71 +1,46 @@
-import { appendAnyfile, deleteAllFile, lowerCase, readAnyFile, sortFile, upperCase, writeAnyFile} from '../problem2.js'
+import {
+  deleteAllFile,
+  lowerCase,
+  readAnyFile,
+  sortFile,
+  upperCase,
+  writeAnyFile,
+} from "../problem2.js";
 
-let fileName = '../lipsum.txt'
-let upperCaseFile = 'upper.txt'
-let lowerCaseFile = 'lower.txt'
-let fileNameFile = 'filename.txt'
-let sortedFileName = 'sorted.txt'
- 
-readAnyFile(fileName,(err,data)=>{
-    if(err){
-        console.log("Error in read lipsum txt",err);
+let fileName = "../lipsum.txt";
+let upperCaseFile = "upper.txt";
+let lowerCaseFile = "lower.txt";
+let fileNameFile = "filename.txt";
+let sortedFileName = "sorted.txt";
+
+readAnyFile(fileName, (err, data) => {
+  if (err) {
+    console.log("Error in read lipsum txt", err);
+  }
+  upperCase(data, upperCaseFile, (err, upperCaseFileName) => {
+    if (err) {
+      console.log("Error in creation of upper.txt", err);
+    } else {
+      lowerCase(data, lowerCaseFile, (err, lowerCaseFileName) => {
+        if (err) {
+          console.log("Error in creation of lower.txt", err);
+        } else {
+          console.log("done creation of lower case");
+          sortFile(data, sortedFileName, (err, sortedFile) => {
+            if (err) {
+              console.log("Error in sorted File Creation", err);
+            } else {
+              deleteAllFile(fileNameFile, (err) => {
+                if (err) {
+                  console.log("Error in deletion");
+                } else {
+                  console.log("Delete done");
+                }
+              });
+            }
+          });
+        }
+      });
     }
-    upperCase(data,upperCaseFile,(err,upperCaseFileName)=>{ 
-        if(err){
-            console.log("Error in creation of upper.txt",err);
-        }   
-        appendAnyfile(upperCaseFileName,fileNameFile,(err)=>{
-            if(err){
-                console.log("Any Error");
-            }
-            else{
-                lowerCase(data,lowerCaseFile,(err,lowerCaseFileName)=>{
-                    if(err){
-                        console.log("Error in creation of lower.txt",err);
-                    } 
-                    else{
-                        console.log("done creation of lower case");     
-                        appendAnyfile(lowerCaseFileName,fileNameFile,(err)=>{
-                            if(err){
-                                console.log("Error in append of lower.txt",err);
-                            } 
-                            else{
-                                sortFile(data,sortedFileName,(err,sortedFile)=>{
-                                    if(err){
-                                        console.log("Error in sorted File Creation",err);
-                                    }
-                                    else{
-                                        console.log("Done Sorted File Creation done");
-                                         appendAnyfile(sortedFile,fileNameFile,(err)=>{
-                                            if(err){
-                                                console.log("Error in append of sorted.txt",err);
-                                            } 
-                                            else{
-                                                console.log("Append of sorted file done");
-                                                deleteAllFile(fileNameFile,(err,msg)=>{
-                                                    if(err){
-                                                        console.log("Error",err)
-                                                    }
-                                                    else{
-                                                          console.log("Deleteion",msg);
-                                                          
-                                                    }
-                                                })   
-                                            }
-                                         })
-                                    }
-                                })
-                            }
-                        })      
-                    }
-                 })
-
-            }
-           
-
-        })
-         
-          
-    })
-
-})
+  });
+});

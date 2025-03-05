556f0b8af1f6f9875908c8f72e95e75fa589843e
Abhishek <warriorsshadow39@gmail.com>
2025-03-05 18:23:49 +0530

    done

--- 9-ninth-drills-https/createServer.js
+++ 9-ninth-drills-https/createServer.js
@@ -77,14 +77,18 @@ function jsonData() {
 }
 
 function htmlCode() {
-  return `<!DOCTYPE html>
+  return `
+  <!DOCTYPE html>
 <html>
-  <head></head>
+  <head>
+  </head>
   <body>
-      <h1>Good programmers write code that humans can understand.</h1>
+      <h1>Any fool can write code that a computer can understand. Good programmers write code that humans can understand.</h1>
       <p> - Martin Fowler</p>
+
   </body>
-</html>`;
+</html>
+  `;
 }
 
 function uuId() {
